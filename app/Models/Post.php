f35feed474ad8c0c2875384525d639bc67eadbe3
soratakeuchi-227 <soratakeuchi0417@gmail.com>
2023-01-10 04:55:37 +0900

    add 記事編集機能

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
-     public function getPaginateByLimit(int $limit_count = 5)
+    protected $fillable = [
+        'title',
+        'body' ,
+        'category_id'
+        ];
+    
+        public function getPaginateByLimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
-
+        
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,21 +3,25 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+　　　　
     </head>
-    <body>
-        <h1 class='title'>
-            {{ $post->title }}
-        </h1>
-        <div class='content'>
-            <div class='content_post'>
-                <h3>本文</h3>
-                <p class='body'>{{ $post->body }}</p>
-            </div>
-    　　</div>
-    　　<div class='footer'>
-        　　<a href="/">戻る</a>
-    　　</div>
+    <body class="antialiased">
+       <h1 class-"title">
+           {{ $post->title }}
+       </h1>
+       <div class="posts">
+           <div class="content_post">
+               <h3>本文</h3>
+               <p class='body'>{{ $post->body }}</p>
+           </div>
+        </div>
+        <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
     </body>
 </html>
